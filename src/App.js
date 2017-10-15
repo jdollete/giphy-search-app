bc444dca90eebc6b03402a9aa2ccaa73ef5af73d
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-10-15 13:32:01 -0400

    Install Redux and redux thunk

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './CSS/App.css';
 import Header from './COMPONENTS/Header';
 import Search from './COMPONENTS/Search';
@@ -6,6 +7,56 @@ import PreviousSearches from './COMPONENTS/PreviousSearches';
 import GifView from './COMPONENTS/GifView';
 
 class App extends Component {
+  // React Lifecycle:
+  // getDefaultProps
+  // getInitialState
+  // componentWillReceiveProps
+  // shouldComponentUpdate
+  // componentWillUpdate
+  // render
+  // componentDidUpdate
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      trendingGifs: ""
+    }
+
+    this.grabTrendingGifs = this.grabTrendingGifs.bind(this);
+    this.setCurrentTrendingState = this.setCurrentTrendingState.bind(this);
+
+  }
+
+  componentWillMount() {
+    const data = this.grabTrendingGifs();
+  }
+
+  componentDidMount() {
+    debugger;
+  }
+
+  grabTrendingGifs() {
+    const _this = this;
+
+    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=N7jGbYOebKSqhTe9Lq0tIz3gpiBU4bRE')
+      .then(response => {
+        console.log("-------------AXIOS---------");
+        _this.setCurrentTrendingState(response.data.data);
+      })
+      .catch(error => {
+        console.log("-------------Error---------");
+        console.log(error);
+      });
+
+  }
+
+  setCurrentTrendingState(data) {
+    this.setState(
+      {trendingGifs: data}
+    );
+  }
+
   render() {
     return (
       <div>
