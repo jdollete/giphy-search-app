064d77302b6dbcd2ed11f3f46c6a21dd0cf29e92
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-10-17 01:40:25 -0400

    Convert components that doesn't use state to stateless functions

--- src/App.js
+++ src/App.js
@@ -7,20 +7,17 @@ import reducers from './reducers';
 import './CSS/App.css';
 import IntroPage from './components/IntroPage';
 
-class App extends Component {
+const App = ({ gifUrl, gifOrigLink }) => {
+  const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
 
-  render() {
-    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
+  return (
+    <Provider store={store}>
+      <div>
+        <IntroPage />
+      </div>
+    </Provider>
 
-    return (
-      <Provider store={store}>
-        <div>
-          <IntroPage />
-        </div>
-      </Provider>
-
-    );
-  }
+  );
 }
 
 export default App;
